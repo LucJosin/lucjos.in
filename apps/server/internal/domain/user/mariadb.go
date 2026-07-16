f8f8d3ef4060daa2f699d97ea27bda0db42da3b5
Lucas Josino <contact@lucasjosino.com>
2026-07-16 20:27:21 -0300

    feat: add user domain

---
+++ apps/server/internal/domain/user/mariadb.go
@@ -0,0 +1,128 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/gofrs/uuid/v5"
+	"github.com/lucjosin/qorv.in/internal/database/mariadb"
+	"github.com/lucjosin/qorv.in/internal/database/tx"
+)
+
+const selectUsersStmt = `
+	SELECT
+		u.id,
+		u.first_name,
+		u.last_name,
+		u.username,
+		u.email,
+		u.password,
+		u.avatar_url,
+		u.email_verified,
+		u.deleted_at,
+		u.created_at,
+		u.updated_at
+	FROM 
+		users u`
+
+type MariaDBRepository struct {
+	db *mariadb.Database
+}
+
+func NewMariaDBRepository(db *mariadb.Database) Repository {
+	return &MariaDBRepository{
+		db: db,
+	}
+}
+
+func (r *MariaDBRepository) FindByID(ctx context.Context, ID uuid.UUID) (User, error) {
+	query := fmt.Sprintf(
+		`%s WHERE u.id = ? AND u.deleted_at IS NULL`,
+		selectUsersStmt,
+	)
+
+	row := r.db.QueryRowContext(ctx, query, ID)
+	entity, err := scanRow(row)
+	if err != nil {
+		return User{}, fmt.Errorf("scanning user: %w", err)
+	}
+
+	return entity, nil
+}
+
+func (r *MariaDBRepository) FindByUsername(ctx context.Context, username string) (User, error) {
+	query := fmt.Sprintf(
+		`%s WHERE u.username = ? AND u.deleted_at IS NULL`,
+		selectUsersStmt,
+	)
+
+	row := r.db.QueryRowContext(ctx, query, username)
+	entity, err := scanRow(row)
+	if err != nil {
+		return User{}, fmt.Errorf("scanning user: %w", err)
+	}
+
+	return entity, nil
+}
+
+func (r *MariaDBRepository) ExistsByID(ctx context.Context, ID uuid.UUID) (bool, error) {
+	query := fmt.Sprintf(`
+	SELECT EXISTS (
+		%s
+		WHERE 
+			u.id = ? AND u.deleted_at IS NULL
+		LIMIT 1
+	)`, selectUsersStmt)
+
+	var exists bool
+	err := r.db.QueryRowContext(ctx, query, ID).Scan(&exists)
+	if err != nil {
+		return false, fmt.Errorf("fetching user: %w", err)
+	}
+
+	return exists, nil
+}
+
+func (r *MariaDBRepository) Create(ctx context.Context, entity User) (User, error) {
+	err := tx.BeginFunc(ctx, r.db, func(tx *sql.Tx) error {
+		now := time.Now()
+
+		query := `
+		INSERT INTO users (
+			first_name,
+			last_name,
+			username,
+			email,
+			password,
+			avatar_url,
+			email_verified,
+			created_at,
+			updated_at
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) RETURNING id`
+
+		args := []any{
+			entity.FirstName,
+			entity.LastName,
+			entity.Username,
+			entity.Email,
+			entity.Password,
+			entity.AvatarURL,
+			entity.EmailVerified,
+			now,
+			now,
+		}
+		row := tx.QueryRowContext(ctx, query, args...)
+
+		err := row.Scan(&entity.ID)
+		if err != nil {
+			return mariadb.ParseError(err, "creating user")
+		}
+
+		entity.CreatedAt = now
+		entity.UpdatedAt = now
+		return nil
+	})
+	return entity, err
+}
---
+++ apps/server/internal/domain/user/service.go
@@ -0,0 +1,55 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/gofrs/uuid/v5"
+	"github.com/lucjosin/qorv.in/internal/errs"
+)
+
+type Service interface {
+	FindByID(ctx context.Context, ID uuid.UUID) (User, error)
+	FindOrCreateByUsername(ctx context.Context, entity User) (User, bool, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{
+		repo: repo,
+	}
+}
+
+func (s *service) FindByID(ctx context.Context, ID uuid.UUID) (User, error) {
+	return s.repo.FindByID(ctx, ID)
+}
+
+func (s *service) FindOrCreateByUsername(ctx context.Context, entity User) (User, bool, error) {
+	existingUser, err := s.repo.FindByUsername(ctx, entity.Username)
+	if err == nil {
+		return existingUser, false, nil
+	}
+	if !errors.Is(err, errs.ErrNotFound) {
+		return User{}, false, fmt.Errorf("checking user existence: %w", err)
+	}
+
+	user, err := s.repo.Create(ctx, entity)
+	if err == nil {
+		return user, true, nil
+	}
+
+	if errors.Is(err, errs.ErrConflict) {
+		// query the DB one more time to retrieve the recent data
+		recentUser, err := s.repo.FindByUsername(ctx, user.Username)
+		if err != nil {
+			return User{}, false, fmt.Errorf("retrieving user after conflict: %w", err)
+		}
+		return recentUser, false, nil
+	}
+
+	return User{}, false, fmt.Errorf("creating user: %w", err)
+}
