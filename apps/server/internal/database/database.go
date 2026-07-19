4ed34cf9f9112b816f7b4406f79f35762c13d658
Lucas Josino <contact@lucasjosino.com>
2026-07-18 21:53:15 -0300

    feat: add CloseRows helper

---
+++ apps/server/internal/database/database.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lucjosin/qorv.in/internal/slogx"
+)
+
+// CloseRows closes the [*sql.Rows], logging a warning if an error occurs.
+func CloseRows(ctx context.Context, rows *sql.Rows) {
+	if rows != nil {
+		if err := rows.Close(); err != nil {
+			slogx.FromCtx(ctx).Error("closing rows", "error", rows.Close())
+		}
+	}
+}
